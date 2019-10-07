7a1256250238b9364a5b6a95e0bb4ebbb8a066c1
Luis Fer <pastillalc@gmail.com>
2019-10-06 20:45:11 -0400

    Tarea matrices commit 1

--- First/main.lua
+++ First/main.lua
@@ -30,3 +30,23 @@ end
 
 print (distancia(4,4,4,2))
 print ("aaaaaaaaaaaa")
+
+local linea = display.newLine(20,10,40,30)
+linea:append(150, 200,100,200,40,10,20,10)
+-- local caudrado = display.newRect(150,150,30,40)
+local poligon = display.newPolygon(0,0,{0,0,150,150,40,180})
+poligon:setFillColor(0.4)
+
+local poligon2 = display.newPolygon(150,150,{0,0,150,150,40,180})
+poligon:setFillColor(1,0,0)
+
+local cuadrado = display.newRect(0,200,80,80)
+cuadrado:setFillColor(0,0,1)
+cuadrado.anchorX = 0
+cuadrado.anchorY = 0
+
+local poligon4 = display.newPolygon(150,150,{0,0,30,30,20,60})
+poligon4:setFillColor(0.4)
+poligon4.XScale= 3
+poligon4.YScale= 3
+poligon4.strokeWidth = 1
---
+++ Tarea_Matrices/main.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+-- 6/10/2019 Luis Fernando Choque Arana
+-- Tarea 1 de Infografia
+-- Trasformaciones de matrices
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+
+function distancia (ax, ay,bx,by)
+  return math.sqrt((math.pow(ax-bx,2)+ (math.pow(ay-by,2)) ))
+end
